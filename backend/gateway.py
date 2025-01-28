80e7e42abfbb2b2b2de98f16cdf96f187698ae58
Ernesto-B <ernesto.barreto@ucalgary.ca>
2025-01-28 00:11:33 -0700

    Basic Gateway made, requirements.txt slightly modified

---
+++ backend/gateway.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+from endpoints.telemetry_api import telemetry_api
+from endpoints.data_analysis import data_analysis_api
+from endpoints.report_gen import report_gen_api
+
+app = Flask(__name__)
+
+# Registering API endpoints
+app.register_blueprint(telemetry_api, url_prefix='/api/telemetry')
+app.register_blueprint(data_analysis_api, url_prefix='/api/data_analysis')
+app.register_blueprint(report_gen_api, url_prefix='/api/report_gen')
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=3000, debug=True)
