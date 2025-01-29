1801d77f47ffb1a5fc4deb351e90ce7eaa0e7ae4
Ernesto-B <ernesto.barreto@ucalgary.ca>
2025-01-28 19:30:25 -0700

    User account deletion added and tests made

--- backend/endpoints/auth.py
+++ backend/endpoints/auth.py
@@ -47,8 +47,8 @@ def delete_account():
     API endpoint for deleting user data from the DB.
     """
     data = request.get_json()
-    username = data.get(username)
-    password = data.get(password)
+    username = data.get('username')
+    password = data.get('password')
     
     if not username or not password:
         return jsonify({"error": "Username and password are required"}), 400
