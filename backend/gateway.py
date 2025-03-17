6c1eec32b387dc7d0597b9333d5bfb793c134776
NavjotSaroa <navjotsaroa8@gmail.com>
2025-03-16 21:34:02 -0600

    Trying more stuff out

--- backend/gateway.py
+++ backend/gateway.py
@@ -4,6 +4,7 @@ from endpoints.request_handler import request_handler
 from endpoints.data_analysis import data_analysis_api
 from endpoints.report_gen import report_gen_api
 from endpoints.auth import auth_api
+import os
 
 app = Flask(__name__)
 
@@ -17,5 +18,7 @@ app.register_blueprint(data_analysis_api, url_prefix='/api/data_analysis')
 app.register_blueprint(report_gen_api, url_prefix='/api/report_gen')
 app.register_blueprint(auth_api, url_prefix='/api/auth')
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get("PORT", 10000))   # 10000 is default for render
+    app.run(host='0.0.0.0', port = port)
